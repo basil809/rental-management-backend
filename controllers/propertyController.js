ebc73ae636cebf73c1de50c1182d2585f189dd55
Basil <your_email@example.com>
2025-12-27 12:33:03 +0300

    fixing the property api

--- controllers/propertyController.js
+++ controllers/propertyController.js
@@ -70,11 +70,8 @@ exports.createProperty = async (req, res) => {
 // READ: Get all properties
 exports.getAllProperties = async (req, res) => {
   try {
-    const properties = await Property.find({});
-    console.log('Properties found:', properties.length);
-    console.log('Connected DB:', mongoose.connection.name);
-    res.json(properties);
-
+    const properties = await Property.find().sort({ createdAt: -1 });
+    res.status(200).json(properties);
   } catch (err) {
     res.status(500).json({ message: 'Failed to fetch properties.' });
   }
